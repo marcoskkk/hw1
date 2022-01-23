0732a9eef2d8b2a41b00fde2eaf5a9c97db435ca
marcoskkk <marcos.kashima@kellogg.northwestern.edu>
2022-01-23 18:22:09 +0000

    v1

--- kmdb.sql
+++ kmdb.sql
@@ -68,13 +68,88 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS mpaa_ratings;
+DROP TABLE IF EXISTS castings;
+
 
 -- Create new tables, according to your domain model
 -- TODO!
 
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  release_year TEXT,
+  mpaa_rating_id INTEGER,
+  director_person_id INTEGER
+  );
+
+CREATE TABLE mpaa_ratings (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  rating TEXT,
+  description TEXT,
+  );
+
+CREATE TABLE people (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+  );
+
+  CREATE TABLE castings (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  actor_person_id INTEGER,
+  movie_id INTEGER,
+  role TEXT
+  );
+
+
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+INSERT INTO mpaa_ratings
+VALUES ("G", "General Audiences");
+VALUES ("PG", "Parental Guidance Suggested");
+VALUES ("PG-13", "Parents Strongly Cautioned");
+VALUES ("R", "Restricted");
+VALUES ("NC-17", "Adults Only");
+
+INSERT INTO people
+VALUES ("Christopher Nolan"); -- 1
+VALUES ("Christian Bale"); -- 2
+VALUES ("Michael Caine"); -- 3
+VALUES ("Liam Neeson"); -- 4
+VALUES ("Katie Holmes"); -- 5
+VALUES ("Gary Oldman"); -- 6
+VALUES ("Heath Ledger"); -- 7
+VALUES ("Aaron Eckhart"); -- 8
+VALUES ("Maggie Gyllenhaal"); -- 9
+VALUES ("Tom Hardy"); -- 10
+VALUES ("Joseph Gordon-Levitt"); -- 11
+VALUES ("Anne Hathaway"); -- 12
+
+INSERT INTO movies
+VALUES ("Batman Begins", "2005", 3, 1);
+VALUES ("The Dark Knight", "2008", 3, 1);
+VALUES ("The Dark Knight Risess", "2012", 3, 1);
+
+INSERT INTO castings
+VALUES (1, 2, "Bruce Wayne");
+VALUES (1, 3, "Alfred");
+VALUES (1, 4, "Ra's Al Ghul");
+VALUES (1, 5, "Rachel Dawes");
+VALUES (1, 6, "Commissioner Gordon");
+VALUES (2, 2, "Bruce Wayne");
+VALUES (2, 7, "Joker");
+VALUES (2, 8, "Harvey Dent");
+VALUES (2, 3, "Alfred");
+VALUES (2, 9, "Rachel Dawes");
+VALUES (3, 2, "Bruce Wayne");
+VALUES (3, 6, "Commissioner Gordon");
+VALUES (3, 10, "Bane");
+VALUES (3, 11, "John Blake");
+VALUES (3, 12, "Selina Kyle");
+
 
 -- Prints a header for the movies output
 .print "Movies"
